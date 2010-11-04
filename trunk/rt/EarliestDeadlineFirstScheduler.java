207bc300bfd9fab60c9e2e52b9fcfce6b9d6ae9c
Vurpan <Vurpan@8bc0da26-5340-b5e3-cc18-bea3724f3a35>
2010-11-04 15:26:47 +0000

    testar EDF...

--- trunk/rt/EarliestDeadlineFirstScheduler.java
+++ trunk/rt/EarliestDeadlineFirstScheduler.java
@@ -96,7 +96,23 @@ public class EarliestDeadlineFirstScheduler extends Scheduler{
     }
   }
   
+  private void rescheduleFirst(AbsoluteTime nextReleaseTime) {
 
+	RMThreadNode threadNode;
+	threadNode = (RMThreadNode) readyList.removeFirst();
+	
+	/* Set next release time */
+	threadNode.nextRelease.add(
+			((PeriodicParameters)threadNode.thread.getReleaseParameters()).getPeriod(), 
+			nextReleaseTime);
+	threadNode.nextRelease.setTime(nextReleaseTime);
+	
+	/* Set next deadline */
+	threadNode.nextRelease.add(threadNode.thread.getReleaseParameters().getDeadline(),
+			 threadNode.deadline);
+		
+	suspendedList.insert(threadNode);
+}
   
   /**
    * Priority driven scheduling. Your task is to make a scheduler by
@@ -106,6 +122,8 @@ public class EarliestDeadlineFirstScheduler extends Scheduler{
 		
     /* Create some objects needed */
     AbsoluteTime absTime = new AbsoluteTime();
+    AbsoluteTime nextReleaseTime = new AbsoluteTime();
+    RelativeTime sleepTime = new RelativeTime();
 	
     RMThreadNode readyThreadNode;
     RLThread readyThread;
@@ -117,9 +135,53 @@ public class EarliestDeadlineFirstScheduler extends Scheduler{
     HighResolutionClock.resetClock();
 		
     while (true) {
-      
-      /* !!! Your code here !!! */
+    	HighResolutionClock.getTime(absTime);
 
+    	/* Move threads to ready queue. */
+    	while (!(suspendedList.isEmpty()) && 
+    		   !(((RMThreadNode)suspendedList.getFirst()).nextRelease.isGreater(absTime))) {
+    		suspendedThreadNode = (RMThreadNode) suspendedList.removeFirst();
+    		suspendedThread = suspendedThreadNode.thread;
+    		System.out.println("Release task: " + suspendedThread.getName() + 
+					" Time: " + absTime.toString() + 
+					" Release time: " + suspendedThreadNode.nextRelease.toString() +
+					" Deadline: " + suspendedThreadNode.deadline.toString());
+    		suspendedThread.setReady();
+    		readyList.insert(suspendedThreadNode);
+    	}
+    	
+    	System.out.println("Ready list: " + readyList.toString());
+    	
+    	/* Reschedule tasks with missed deadlines */
+    	while ((!(readyList.isEmpty())) && 
+    			(!(((RMThreadNode) readyList.getFirst()).deadline.isGreater(absTime)))) {
+    		System.out.println("Missed deadline: " + readyList.getFirst().thread.getName() + 
+    							" Time: " + absTime.toString() + 
+    							" Deadline: " + ((RMThreadNode) readyList.getFirst()).deadline.toString());
+    		((RMThreadNode) readyList.getFirst()).thread.getDeadlineMissHandler().handleAsyncEvent();
+    		rescheduleFirst(nextReleaseTime);
+    	}
+    	
+    	/* Run task with closest deadline. */
+    	if (!(readyList.isEmpty())) {
+	    	
+	    	readyThreadNode = (RMThreadNode) readyList.getFirst();
+	    	readyThread = readyThreadNode.thread;
+	       	fireThread(readyThread);
+	       	
+	       	/* Task running... */
+	    	
+	    	/* Reschedule if finished. */
+	    	if (readyThread.isFinished()) {
+	    		System.out.println("Task finished: " + readyThread.getName());
+	    		rescheduleFirst(nextReleaseTime);
+	    	}
+    	
+    	} else {
+    		((RMThreadNode) suspendedList.getFirst()).nextRelease.subtract(absTime, sleepTime);
+    		System.out.println("sleeping: " + sleepTime.toString());
+        	this.sleep(sleepTime);
+    	}
     }
   }
 }	
